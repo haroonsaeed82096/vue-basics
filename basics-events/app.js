b60f2e4479e03899e69fa76d4c4369ef34033461
haroonsaeed82096 <30671721+haroonsaeed82096@users.noreply.github.com>
2021-07-12 15:02:59 +0200

    name display

--- basics-events/app.js
+++ basics-events/app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
     add() {
       this.counter = this.counter + 1;
     },
